eb186028ab94c80d79476106d6ebdd486da32162
mike ¯\_(ツ)_/¯ <1847895+mikerapin@users.noreply.github.com>
2023-11-05 21:32:51 -0500

    point to new api endpoint

--- packages/client/src/util/http.ts
+++ packages/client/src/util/http.ts
@@ -2,7 +2,7 @@ export const getBaseUrl = () => {
   let url;
   switch (process.env.NODE_ENV) {
     case 'production':
-      url = 'https://www.justreadcomics.com';
+      url = 'https://api.justreadcomics.com';
       break;
     case 'test':
       url = 'http://localhost:8080';
